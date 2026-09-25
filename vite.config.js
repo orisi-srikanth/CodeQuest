f8670401316c981f20b0385739312f45832d10d3
srikanth <srikanthorisi62@gmail.com>
2026-09-25 09:09:22 +0530

    Fix duplicate Vite config exports

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { fileURLToPath, URL } from 'node:url'
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/CodeQuest/',
-})
 
 // https://vite.dev/config/
 export default defineConfig({
